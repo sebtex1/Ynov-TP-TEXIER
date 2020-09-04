a01ecf171bd5e3f016e09602568f41ffdb0bcb09
SebTex <sebastien.tex6@hotmail.fr>
2020-09-04 09:19:02 +0200

    top repositories fini

--- projet_GitHub/projet.py
+++ projet_GitHub/projet.py
@@ -1,15 +1,16 @@
 import requests
 import json
-from bs4 import BeautifulSoup
 
 def getRepositories():
     repositories = requests.get("https://api.github.com/repositories") 
-    parse = BeautifulSoup(repositories.text)
-    repositoriesJSON = json.loads(str(parse))
+    repositoriesJSON = json.loads(repositories.text)
     for doc in repositoriesJSON:
-        print(doc["name"])
-        print(doc["description"] is not None and doc["description"] or "no description") 
         open('./projet_GitHub/repositories.txt', "a").write("Title: " + doc["name"] + "\n" + "Description: " + (doc["description"] is not None and doc["description"] or "no description") + "\n")
-    # return 
+    
+def getTopRepositories():
+    repositories=requests.get("https://api.github.com/search/repositories?sort=stars&q=created:2020-01-01&per_page=100")
+    repositoriesJSON = json.loads(repositories.text)["items"]
+    for doc in repositoriesJSON:
+        open('./projet_GitHub/topRepositories.txt', "a", encoding="utf8").write("Title: " + doc["name"] + "\n" + "Description: " + (doc["description"] is not None and doc["description"] or "no description") + "\n" + "Number of stars: " + str(doc["stargazers_count"]) + "\n")
 
-getRepositories()
+getTopRepositories()
